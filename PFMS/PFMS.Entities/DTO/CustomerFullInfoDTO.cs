4953db8b3811e24245f9856c7613c5e80cc6355b
Maryan Mayher <mr.maykher@gmail.com>
2016-10-31 13:40:37 +0200

    added dto for fu customer info

---
+++ PFMS/PFMS.Entities/DTO/CustomerFullInfoDTO.cs
@@ -0,0 +1,15 @@
+﻿using PFMS.Entities.AbstractDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFMS.Entities.DTO
+{
+    public class CustomerFullInfoDTO : BaseFullPersonInfoDTO
+    {
+        public string AccountNumber { get; set; }
+        public IEnumerable<OrderDTO> Orders { get; set; }
+    }
+}
---
+++ PFMS/PFMS.Repositories/Abstract/IProductsRepository.cs
@@ -0,0 +1,13 @@
+﻿using PFMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFMS.Repositories.Abstract
+{
+    public interface IProductsRepository: IRepository<Product>
+    {
+    }
+}

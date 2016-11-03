362d4ad47754c5136c58aee38d0912725880cf8f
Maryan Mayher <mr.maykher@gmail.com>
2016-11-03 17:53:04 +0200

    ibase repo

---
+++ PFMS/PFMS.Repositories/Abstract/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFMS.Repositories.Abstract
+{
+    public interface IBaseRepository<T>
+    {
+        void Delete<TEntity>(Func<TEntity, bool> criteria) where TEntity : class;
+        IEnumerable<TEntity> Get<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class;
+        TEntity GetSingle<TEntity>(Func<TEntity, bool> criteria = null) where TEntity : class;
+        TEntity Insert<TEntity>(TEntity entity) where TEntity : class;
+        TEntity Update<TEntity>(TEntity entity) where TEntity : class;
+
+
+    }
+}
